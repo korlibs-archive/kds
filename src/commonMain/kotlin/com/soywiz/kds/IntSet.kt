6fdd826935b91af7a9ad456e29bc8503a7747e9c
soywiz <soywiz@gmail.com>
2018-12-14 22:02:11 +0100

    More work on IntSet

--- src/commonMain/kotlin/com/soywiz/kds/IntSet.kt
+++ src/commonMain/kotlin/com/soywiz/kds/IntSet.kt
@@ -1,21 +1,47 @@
 package com.soywiz.kds
 
-class IntSet {
-	private val data = IntMap<Unit>()
+/**
+ * A [Set] structure representing a set of [Int] without boxing.
+ *
+ * Internally uses [IntMap] behind the scenes. Implements [MutableSet].
+ */
+class IntSet : MutableSet<Int> {
+    private var data = IntMap<Unit>()
+    override val size get() = data.size
 
-	fun clear() = run { data.clear() }
-	fun add(item: Int) = run { data[item] = Unit }
-	fun addAll(vararg items: Int) = run { for (item in items) add(item) }
-	fun addAll(items: Iterable<Int>) = run { for (item in items) add(item) }
-	operator fun contains(item: Int) = item in data
-	fun remove(item: Int) = run { data.remove(item) }
+    override fun containsAll(elements: Collection<Int>): Boolean = elements.all { it in data }
+    override fun isEmpty(): Boolean = size == 0
+    override fun iterator(): MutableIterator<Int> {
+        val iterator = data.keys.iterator()
+        return object : MutableIterator<Int> {
+            override fun hasNext(): Boolean = iterator.hasNext()
+            override fun next(): Int = iterator.next()
+            override fun remove() = TODO()
+        }
+    }
 
-	operator fun plusAssign(value: Int) = run { add(value); Unit }
-	operator fun minusAssign(value: Int) = run { remove(value); Unit }
+    override fun clear() = run { data.clear() }
+    override fun add(element: Int): Boolean = data.set(element, Unit) == null
+    override fun addAll(elements: Collection<Int>): Boolean = elements.any { add(it) }
+    override fun removeAll(elements: Collection<Int>): Boolean = elements.any { remove(it) }
+    override fun retainAll(elements: Collection<Int>): Boolean {
+        val oldSize = this.size
+        val other = IntMap<Unit>()
+        for (it in elements) if (it in this) other[it] = Unit
+        this.data = other
+        return oldSize == this.size
+    }
 
-	override fun toString(): String {
-		return "[${data.keys.joinToString(", ")}]"
-	}
+    fun addAll(vararg elements: Int) = run { for (item in elements) add(item) }
+    fun addAll(elements: Iterable<Int>) = run { for (item in elements) add(item) }
+
+    override operator fun contains(element: Int) = element in data
+    override fun remove(element: Int) = data.remove(element)
+
+    operator fun plusAssign(value: Int) = run { add(value); Unit }
+    operator fun minusAssign(value: Int) = run { remove(value); Unit }
+
+    override fun toString(): String = "[${data.keys.joinToString(", ")}]"
 }
 
 fun intSetOf(vararg values: Int) = IntSet().apply { for (value in values) add(value) }
--- src/commonTest/kotlin/com/soywiz/kds/IntSetTest.kt
+++ src/commonTest/kotlin/com/soywiz/kds/IntSetTest.kt
@@ -5,14 +5,22 @@ import kotlin.test.assertEquals
 
 class IntSetTest {
     @Test
-    fun test() {
+    fun testSmoke() {
         val set = intSetOf(1, 2, 4)
+        assertEquals(3, set.size)
+
         assertEquals(false, 0 in set)
         assertEquals(true, 1 in set)
         assertEquals(true, 2 in set)
         assertEquals(false, 3 in set)
         assertEquals(true, 4 in set)
         assertEquals(false, 5 in set)
+
+        set.remove(2)
+        assertEquals(2, set.size)
+        assertEquals(true, 1 in set)
+        assertEquals(false, 2 in set)
+        assertEquals(true, 4 in set)
     }
 
     @Test
@@ -20,5 +28,10 @@ class IntSetTest {
         assertEquals("[]", intSetOf().toString())
         assertEquals("[1, 2, 4]", intSetOf(1, 2, 4).toString())
         assertEquals("[1, 2, 4]", intSetOf(4, 2, 1).toString())
+    }
+
+    @Test
+    fun test2() {
+        assertEquals(listOf(1, 2, 3, 4), intSetOf(1, 2, 3, 4).toList().sorted())
     }
 }
