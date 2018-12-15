9f90afd8ffbe5428e0d104770054f796638d651f
soywiz <soywiz@gmail.com>
2018-12-15 13:06:36 +0100

    Declare MapList related classes as extension methods instead of an actual class

--- src/commonMain/kotlin/com/soywiz/kds/MapList.kt
+++ src/commonMain/kotlin/com/soywiz/kds/MapList.kt
@@ -1,43 +1,38 @@
 package com.soywiz.kds
 
-class MapList<K, V>() : Iterable<Pair<K, List<V>>> {
-	override fun iterator(): Iterator<Pair<K, List<V>>> = map.entries.map { it.key to it.value }.iterator()
+typealias MapList<K, V> = Map<K, List<V>>
+typealias MutableMapList<K, V> = MutableMap<K, ArrayList<V>>
+typealias LinkedHashMapList<K, V> = LinkedHashMap<K, ArrayList<V>>
 
-	fun flatMapIterator(): Iterator<Pair<K, V>> =
-		map.entries.flatMap { item -> item.value.map { item.key to it } }.iterator()
+fun <K, V> MapList<K, V>.getFirst(key: K): V? = this[key]?.firstOrNull()
+fun <K, V> MapList<K, V>.getLast(key: K): V? = this[key]?.lastOrNull()
 
-	constructor(items: List<Pair<K, V>>) : this() {
-		for ((k, v) in items) append(k, v)
-	}
+fun <K, V> MapList<K, V>.flatMapIterator(): Iterator<Pair<K, V>> =
+	entries.flatMap { item -> item.value.map { item.key to it } }.iterator()
 
-	constructor(items: Map<K, V>) : this() {
-		for ((k, v) in items) append(k, v)
-	}
+fun <K, V> MutableMapList<K, V>.append(key: K, value: V): MutableMapList<K, V> {
+	getOrPut(key) { arrayListOf() }
+	this[key]!! += value
+	return this
+}
 
-	constructor(items: MapList<K, V>) : this() {
-		for ((k, values) in items) for (v in values) append(k, v)
-	}
+fun <K, V> MutableMapList<K, V>.replace(key: K, value: V): MutableMapList<K, V> {
+	remove(key)
+	append(key, value)
+	return this
+}
 
-	val map = LinkedHashMap<K, ArrayList<V>>()
+fun <K, V> MutableMapList<K, V>.appendAll(vararg items: Pair<K, V>): MutableMapList<K, V> = this.apply { for ((k, v) in items) append(k, v) }
+fun <K, V> MutableMapList<K, V>.replaceAll(vararg items: Pair<K, V>): MutableMapList<K, V> = this.apply { for ((k, v) in items) replace(k, v) }
 
-	fun append(key: K, value: V): MapList<K, V> {
-		map.getOrPut(key) { arrayListOf() }
-		map[key]!! += value
-		return this
-	}
+fun <K, V> linkedHashMapListOf(vararg items: Pair<K, V>): MutableMapList<K, V> = LinkedHashMapList<K, V>().apply {
+	for ((k, v) in items) append(k, v)
+}
 
-	fun replace(key: K, value: V): MapList<K, V> {
-		map.remove(key)
-		append(key, value)
-		return this
-	}
+fun <K, V> LinkedHashMapList(items: List<Pair<K, V>>): MutableMapList<K, V> = LinkedHashMapList<K, V>().apply {
+	for ((k, v) in items) append(k, v)
+}
 
-	fun appendAll(vararg items: Pair<K, V>): MapList<K, V> = this.apply { for ((k, v) in items) append(k, v) }
-	fun replaceAll(vararg items: Pair<K, V>): MapList<K, V> = this.apply { for ((k, v) in items) replace(k, v) }
-
-	fun get(key: K) = map[key]
-
-	fun getFirst(key: K) = map[key]?.firstOrNull()
-
-
+fun <K, V> LinkedHashMapList(items: MapList<K, V>): MutableMapList<K, V> = LinkedHashMapList<K, V>().apply {
+	for ((k, values) in items) for (v in values) append(k, v)
 }
